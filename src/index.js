013bfdc41dda0604f3e32df90defe491ef98a065
Catalina Ulloa <catalinauluchs@gmail.com>
2023-07-14 12:43:08 -0400

    Added forecast
    WIP, for some reason it shows the week inversed

--- src/index.js
+++ src/index.js
@@ -21,29 +21,86 @@ function formatDate(timestamp) {
  return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+
+  let date = new Date(timestamp * 1000);
+    let days = [
+    "Sun",
+    "Mon",
+    "Tue",
+    "Wed",
+    "Thu",
+    "Fri",
+    "Sat",
+  ];
+
+  let day = days[date.getDay()];
+  return day;
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+  let forecastElement = document.querySelector("#weather-forecast");
+
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay , index) {
+    if (index < 6) {
+    forecastHTML =
+      forecastHTML +
+      `
+      <div class="col">
+                <div class="week-day">${formatDay(forecastDay.time * 1000)}</div>
+                <img src="/images/${forecastDay.condition.icon}.svg" alt="">
+                <div class="week-temp">
+                    <span class="min">${Math.round(forecastDay.temperature.minimum)}°</span> <span class="max"><strong>${Math.round(forecastDay.temperature.maximum)}°</strong></span>
+                </div>
+            </div>
+  `;
+  }
+});
+
+  
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  console.log(coordinates)
+  let apiKey = "b234ft44ac33162fe6fb033o240936aa";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${coordinates.lon}&lat=${coordinates.lat}&key=${apiKey}
+`
+axios.get(apiUrl).then(displayForecast);
+
+}
+
 function displayTemp(response) {
-  let minMax = document.querySelector(".minMax");
   let cityDisplay = document.querySelector("#city");
   let temp = document.querySelector(".current-temp");
+  let minMax = document.querySelector(".minMax");
   let descriptionElement = document.querySelector("#description");
   let dateElement = document.querySelector("#date");
   let iconCode = response.data.weather[0].icon;
   let iconImg = document.querySelector("#icon");
+  let minTemp = response.data.main.temp_min;
+  let maxTemp = response.data.main.temp_max;
 
   celsiusTemp = response.data.main.temp;
 
   cityDisplay.innerHTML = response.data.name;
   temp.innerHTML = `${Math.round(celsiusTemp)}`;
   minMax.innerHTML = `${Math.round(
-    response.data.main.temp_min
-  )}°c | ${Math.round(response.data.main.temp_max)}°c`;
+    minTemp
+  )}° <strong>${Math.round(maxTemp)}</strong>°`;
   descriptionElement.innerHTML = response.data.weather[0].description;
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
-   
+  windPerc.innerHTML = `${Math.round(response.data.wind.speed * 3.6)}`;
   iconImg.setAttribute("src", `/images/${iconCode}.svg`);
   iconImg.setAttribute("alt", response.data.weather[0].description);
-
+  
+  getForecast(response.data.coord);
 }
+
 
 function search(event) {
   event.preventDefault();
@@ -92,15 +149,13 @@ function convertC(event) {
   tempInd.innerHTML = "°C"
   celsiusBtn.classList.remove("inactive-btn")
   celsiusBtn.classList.add("active-btn")
-  fahrenheitBtn.classList.add("inactive-btn")
+  fahrenheitBtn.classList.add("active-btn")
 }
 
 let celsiusTemp = null;
-
 let fahrenheitBtn = document.querySelector("#fahrenheit-btn");
-fahrenheitBtn.addEventListener("click", convertF)
-
 let celsiusBtn = document.querySelector("#celsius-btn");
-celsiusBtn.addEventListener("click", convertC)
 
+fahrenheitBtn.addEventListener("click", convertF)
+celsiusBtn.addEventListener("click", convertC)
 cityBtn.addEventListener("click", search);
